c69d92620494227ee4f9ebb3a2fc16a2e6d3f01b
Tenzing Nyima <tnyima19@gmail.com>
2024-03-01 19:04:22 -0500

    added LeftSideBar to NotesViewPage

--- client/src/pages/NotesViewPage.js
+++ client/src/pages/NotesViewPage.js
@@ -1,12 +1,29 @@
+import React from 'react';
 import Notes from '../components/Notes';
-
+import LeftSideBar from '../components/LeftSideBar';
 const NotesViewPage = () => {
 
-  return (
-    <div>
-      <Notes />
-    </div>
-  );
+
+ return (
+   <div>
+       <h1>Notes website</h1>
+       <div className='container-fluid'>
+         <div className='row'>
+         <div className='col-md-3'>
+           <LeftSideBar />
+         </div>
+      
+
+
+      
+     <div className='col-md-9'>
+       <Notes />
+     </div>
+     </div>
+     </div>
+   </div>
+ );
 };
+
 
 export default NotesViewPage;
