fff417c9ab666b8177756cedc6f6fd7550a05554
Codemaster <72892949+Albertcodemaster@users.noreply.github.com>
2024-05-16 12:37:35 -0400

    Add files via upload

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useRef } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css'; // Include the Quill CSS
-
-const Notes = () => {
-  const editorRef = useRef(null); // Reference to the editor element
-
-  useEffect(() => {
-    if (editorRef.current) {
-      const editor = new Quill(editorRef.current, {
-        theme: 'snow', // Specify theme
-        modules: {
-          toolbar: [
-            [{ header: [1, 2, false] }],
-            ['bold', 'italic', 'underline'],
-            ['image', 'code-block']
-          ]
-        }
-      });
-
-      // Set initial editor content if value exists
-    //   if (value) {
-    //     editor.clipboard.dangerouslyPasteHTML(value);
-    //   }
-
-      // Handle editor content changes
-    //   editor.on('text-change', () => {
-    //     onChange(editor.root.innerHTML); // Pass HTML content up to parent component
-    //   });
-    }
-  }, ); // Re-run effect if `onChange` or `value` changes
-
-  return <div ref={editorRef} />;
-};
-
-export default Notes;
-=======
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
@@ -205,4 +167,3 @@ const Notes = () => {
 };
 
 export default Notes;
->>>>>>> cacb9ba (Pushing all changes to the main branch fully functional and completed)
