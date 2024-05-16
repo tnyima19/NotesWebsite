fc83add32d18c9db70f27d3c04ff3303a6f5b69b
Codemaster <72892949+Albertcodemaster@users.noreply.github.com>
2024-05-16 12:37:04 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import NotesViewPage from "./pages/NotesViewPage";
-
-
-import "./App.css";
-
-// function Navigation(props) {
-//   return (
-//     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-//       <div className="container-fluid">
-//         <Link className="navbar-brand" to="/">
-//           Micro Blog
-//         </Link>
-//         <ul className="navbar-nav me-auto">
-//           <li className="nav-item">
-//             <NavLink className="nav-link" to="/posts/new">
-//               Create a Micro Post
-//             </NavLink>
-//           </li>
-//           <li className="nav-item">
-//             <NavLink className="nav-link" to="/Login">
-//               Logout
-//             </NavLink>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// }
-
-function App() {
-  return (
-    <BrowserRouter>
-      {/* <Navigation /> */}
-      <div className="container-xl text-center">
-        <div className="row justify-content-center">
-          <Routes>
-            <Route path="/Login" element={<LoginPage />} />
-            <Route path="/Signup" element={<SignUpPage />} />
-            <Route path="" element={<NotesViewPage />} />
-
-            
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-=======
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -94,4 +39,3 @@ function App() {
 }
 
 export default App;
->>>>>>> cacb9ba (Pushing all changes to the main branch fully functional and completed)
